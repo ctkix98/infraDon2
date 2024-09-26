1695ffa973ea4ad501c610d8ae3150c544b746bb
ctkix98 <cedrine.tille@gmail.com>
2024-09-26 11:49:10 +0200

    Add new page

--- noSQL/src/App.vue
+++ noSQL/src/App.vue
@@ -13,6 +13,7 @@ import HelloWorld from './components/HelloWorld.vue'
       <nav>
         <RouterLink to="/">Home</RouterLink>
         <RouterLink to="/about">About</RouterLink>
+        <RouterLink to="/cedrine">Me</RouterLink>
       </nav>
     </div>
   </header>
