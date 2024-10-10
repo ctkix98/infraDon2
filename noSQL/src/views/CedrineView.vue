e472f81e316fddae55c9105050f79eea2cf1d20a
Cedrine Tille <cedrine.tille@gmail.com>
2024-10-10 11:51:35 +0200

    Add fonction to get name of bikes

--- noSQL/src/views/CedrineView.vue
+++ noSQL/src/views/CedrineView.vue
@@ -15,7 +15,7 @@ export default {
       const $pouchDB = new PouchDB('http://localhost:5984/bikeshop');
       if ($pouchDB) {
         console.log('Connection with PouchDB established successfully')
-        console.log($pouchDB)
+        //console.log($pouchDB)
       } else {
         console.warn('Something went wrong')
       }
@@ -25,15 +25,54 @@ export default {
  
     fetchData() {
       this.storage?.allDocs().then(function (doc) {
+        console.log("Ce sont toutes les données")
         console.log(doc)
       })
+    },
+
+    //Récupère les infos générales de la DB
+    getInfo(){
+    this.storage?.info().then(function(info){
+      console.log("ce sont les infos")
+      console.log(info)
+    })
+    },
+
+    //Récupérer
+    getName(){
+      this.storage?.allDocs({include_docs :true}).then(result => {
+        this.datas = result.rows.map(row =>row.doc);
+        console.log("Les noms des vélos")
+        this.datas.forEach((doc:any)=>{
+          doc.velos.forEach((velo: any)=>{
+            console.log(`${velo.nom}`);
+          })
+          
+        })
+      })
+
+      
+      
+      //console.log(`Ce sont les données ${donnees}`);
+      //this.storage?.get('5950d965e89ee73edf2ed20ed2003cc3').then(function(doc){
+        //console.log(doc)
+      //})
+      //console.log("j'imprime un nom ici...")
+      //console.log(this.storage?.name)
+
+
     }
+
+
   },
+
 
   // Hook mounted pour initialiser la base de données lors du montage du composant
   mounted() {
     this.initDatabase();
     this.fetchData();
+    this.getInfo();
+    this.getName();
   },
 };
 </script>
