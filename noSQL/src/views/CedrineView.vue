97e7501ff4e7568a182f3f0781d23a2ae3d0099d
Cedrine Tille <cedrine.tille@gmail.com>
2024-10-31 10:45:50 +0100

    Create a new doc

--- noSQL/src/views/CedrineView.vue
+++ noSQL/src/views/CedrineView.vue
@@ -4,93 +4,116 @@ export default {
   data() {
     return {
       datas: [] as any[], // Stockage des documents, et mettre ici que c'est un tableau vide, de n'importe quel type
-      databaseReference: null, // Référence à la base de données
-      storage: null as PouchDB.Database | null
-    };
+      storage: null as PouchDB.Database | null // Référence à la base de données
+    }
   },
 
   methods: {
     // Initialisation de la base de données
     initDatabase() {
-      const $pouchDB = new PouchDB('http://localhost:5984/bikeshop');
-      if ($pouchDB) {
+      const pouchDB = new PouchDB('http://localhost:5984/bikeshop')
+      if (pouchDB) {
         console.log('Connection with PouchDB established successfully')
         //console.log($pouchDB)
       } else {
         console.warn('Something went wrong')
       }
- 
-      this.storage = $pouchDB
+      this.storage = pouchDB
     },
- 
+
     fetchData() {
       this.storage?.allDocs().then(function (doc) {
-        console.log("Ce sont toutes les données")
+        console.log('Ce sont toutes les données')
         console.log(doc)
       })
     },
 
     //Récupère les infos générales de la DB
-    getInfo(){
-    this.storage?.info().then(function(info){
-      console.log("ce sont les infos")
-      console.log(info)
-    })
+    getInfo() {
+      this.storage?.info().then(function (info) {
+        console.log('ce sont les infos')
+        console.log(info)
+      })
     },
 
     //Récupérer
-    getName(){
-      
-      const self = this; //donc pour récupérer l'objet, on fait une constante
+    getName() {
+      const self = this //donc pour récupérer l'objet, on fait une constante
       // ici dans mon component
-      this.storage?.allDocs({include_docs :true}).then(result => {
+      this.storage?.allDocs({ include_docs: true }).then((result) => {
         // ici je suis dans l object base de donnees
         // this = base de donnees
-        self.datas = result.rows.map(row =>row.doc);
-        console.log("Les noms des vélos")
-        this.datas.forEach((doc:any)=>{
-          doc.velos.forEach((velo: any)=>{
-            console.log(`${velo.nom}`);
+        self.datas = result.rows.map((row) => row.doc)
+        console.log('Les noms des vélos')
+        this.datas.forEach((doc: any) => {
+          doc.velos.forEach((velo: any) => {
+            console.log(`${velo.nom}`)
           })
-          
         })
       })
 
-      
-      
       //console.log(`Ce sont les données ${donnees}`);
       //this.storage?.get('5950d965e89ee73edf2ed20ed2003cc3').then(function(doc){
-        //console.log(doc)
+      //console.log(doc)
       //})
       //console.log("j'imprime un nom ici...")
       //console.log(this.storage?.name)
-
-
+    },
+    //Créer un nouveau document
+    createNewDoc(newDoc: any) {
+      this.storage?.post(newDoc)
+        .then((response) => {
+          console.log('Document ajouté avec succès :', response)
+          if (response?.ok) {
+            this.datas.push({ ...newDoc, _id: response.id, _rev: response.rev }) // Mise à jour locale avec ID et révision
+          }
+        })
+        .catch((error) => {
+          console.error("Erreur lors de l'ajout du document :", error)
+        })
+    },
+    getFakeDoc() {
+      const nouveauVelo = {
+        velos: [
+          {
+            nom: 'Velo de Course Speedster',
+            description: 'Parfait pour les amateurs de vitesse en milieu urbain.',
+            prix: 1300,
+            couleur: 'Jaune',
+            avis: [
+              {
+                utilisateur: 'Marc',
+                commentaire: 'Léger et rapide, idéal pour les trajets en ville.'
+              }
+            ]
+          }
+        ]
+      }
+      return nouveauVelo;
     }
-
-
   },
-
 
   // Hook mounted pour initialiser la base de données lors du montage du composant
   mounted() {
-    this.initDatabase();
-    this.fetchData();
-    this.getInfo();
+    this.initDatabase()
+    this.fetchData()
+    this.getInfo()
+    this.getName()
+    //this.createNewDoc(this.getFakeDoc())
     this.getName();
-  },
-};
+  }
+}
 </script>
 
 <template>
   <div class="cedrine">
     <h1>This is a Cedrine page</h1>
   </div>
-  <!--<div>
-    <button @click="Inc">
-      Count is: {{ total }}
+  <div>
+    <button @click="createNewDoc(getFakeDoc())">
+      Ajouter un document
     </button>
-  </div> -->
+  </div>
 </template>
 
 <style>
